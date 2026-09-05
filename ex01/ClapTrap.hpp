5aa97db3740a706e45b4bc76cde6c155a12ef642
abenrach <abenrach@f1br10s1.paris.42.school>
2026-09-05 08:47:11 +0200

    ex01 added

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenrach <abenrach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/31 12:14:04 by abenrach          #+#    #+#             */
+/*   Updated: 2026/09/05 07:52:13 by abenrach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+# include <iostream>
+# include <string>
+
+class   ClapTrap
+{
+    protected:
+        std::string     name;
+        unsigned int    hit_point;
+        unsigned int    energy_point;
+        unsigned int    attack_damage;
+    public:
+        ClapTrap(std::string name);
+        ClapTrap(const ClapTrap &src);
+        ClapTrap    &operator=(const ClapTrap &src);
+        void        attack(const std::string &target);
+        void        takeDamage(unsigned int amount);
+        void        beRepaired(unsigned int amount);
+        ~ClapTrap(void);
+};
+
+#endif
